eb721cfc2acb9def4d5dbe96556c13f21b466629
Philip <park.j.philip@gmail.com>
2017-07-29 23:02:18 -0400

    initialize work on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,39 @@
 class User < ApplicationRecord
+  attr_reader :password
+
+  validates :email, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: {minimum: 6}, allow_nil: true
+
+  after_initialize :ensure_session_token
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
+
+  def generate_session_token
+    SecureRandom.base64
+  end
 end
