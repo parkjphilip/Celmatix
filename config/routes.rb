a86f160ff0cdb8a9b3ba1bf07945ac8f7bc62f93
Philip <park.j.philip@gmail.com>
2017-07-29 22:56:49 -0400

    initial setup of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: {formats: :json} do
+    resources :users, only [:index, :create]
+    resource :session, only [:create, :destroy]
+    resources :products, only [:index, :create, :show, :destroy, :update]
+    resources :cart, only [:create, :show, :update]
+    resources :order, only [:index, :show]
+  end
 end
