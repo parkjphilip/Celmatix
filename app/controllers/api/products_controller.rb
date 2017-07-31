86cdaf35497eaf8071ed528dbca9da98fde06c8f
Philip <park.j.philip@gmail.com>
2017-07-31 17:28:00 -0400

    add initial code for products controller

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,21 @@
 class Api::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      render "api/products/show"
+    else
+      render json: @product.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :brand, :model, :sku, :price, :desc)
+  end
 end
