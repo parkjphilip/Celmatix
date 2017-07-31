f5d938d2208a2a90db7123d9934e31ba86f05dc1
Philip <park.j.philip@gmail.com>
2017-07-30 21:52:45 -0400

    migrate order items table

--- db/migrate/20170731014926_create_order_items.rb
+++ db/migrate/20170731014926_create_order_items.rb
@@ -1,8 +1,13 @@
 class CreateOrderItems < ActiveRecord::Migration[5.0]
   def change
     create_table :order_items do |t|
+      t.integer :product_id, null: false
+      t.integer :quantity, null: false
+      t.integer :shopping_cart_id, null: false
 
       t.timestamps
     end
+    add_index :order_items, :product_id, unique: true
+    add_index :order_items, :shopping_cart_id, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170731014123) do
+ActiveRecord::Schema.define(version: 20170731014926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 20170731014123) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_carts_on_user_id", unique: true, using: :btree
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer  "product_id",       null: false
+    t.integer  "quantity",         null: false
+    t.integer  "shopping_cart_id", null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["product_id"], name: "index_order_items_on_product_id", unique: true, using: :btree
+    t.index ["shopping_cart_id"], name: "index_order_items_on_shopping_cart_id", unique: true, using: :btree
   end
 
   create_table "orders", force: :cascade do |t|
