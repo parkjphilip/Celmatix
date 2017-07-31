f0555f15aa67344cad04b082030c9103f1783fd7
Philip <park.j.philip@gmail.com>
2017-07-30 22:41:35 -0400

    add code to entry file

---
+++ frontend/entry.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<h2>Welcome to Celmatix Challenge</h2>, root);
+});
