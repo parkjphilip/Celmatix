f03aa8b26438f4b06d3382a5632f5dbb1ccea493
Philip <park.j.philip@gmail.com>
2017-07-30 22:44:11 -0400

    add webpack file

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/entry.jsx",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  plugins: plugins,
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
