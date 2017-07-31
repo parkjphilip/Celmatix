43e0251389b35da06249f6d621fcf829bf949564
Philip <park.j.philip@gmail.com>
2017-07-30 22:36:37 -0400

    add methods for sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,47 @@
+class SessionsController < ApplicationController
+  def create
+		email = params[:user][:email]
+		password = params[:user][:password]
+		@user = User.find_by_credentials(email, password)
+    if @user
+			login(@user)
+			render "api/users/_user"
+		else
+			case
+			when user == "" && password != ""
+				render(
+					json: ["Username can't be blank"],
+					status: 401
+				)
+			when user != "" && password == ""
+				render(
+					json: ["Password can't be blank"],
+					status: 401
+				)
+			when user == "" && password == ""
+				render(
+					json: ["Username & password can't be blank"],
+					status: 401
+				)
+			else
+				render(
+					json: ["Invalid login credentials- please try again"],
+					status: 401
+				)
+			end
+		end
+	end
+
+	def destroy
+		@user = current_user
+		if @user
+			logout
+			render "api/users/show"
+		else
+			render(
+        json: ["Nobody signed in"],
+        status: 404
+      )
+		end
+	end
+end
