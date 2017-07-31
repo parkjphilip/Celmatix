a53bd085b4dfeed6e912d19adee115cefadf0fbb
Philip <park.j.philip@gmail.com>
2017-07-31 00:09:31 -0400

    add session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,43 @@
+import * as APIUtil from '../util/session_api_util';
+import { hashHistory } from 'react-router';
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+export const RESET_ERRORS = "RESET_ERRORS";
+
+export const signup = user => dispatch => (
+  APIUtil.signup(user)
+    .then(user => dispatch(receiveCurrentUser(user)),
+      err => dispatch(receiveErrors(err.responseJSON)))
+);
+
+export const login = user => dispatch => {
+  return (
+  APIUtil.login(user)
+    .then(user => dispatch(receiveCurrentUser(user)),
+      err => dispatch(receiveErrors(err.responseJSON)))
+    );
+};
+
+export const logout = () => dispatch => {
+  return (APIUtil.logout().then(user => dispatch(receiveCurrentUser(null))));
+                          // .then(hashHistory.push(`/`)));
+};
+
+export const receiveCurrentUser = currentUser => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+  };
+};
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
+export const resetErrors = () => {
+  return {
+    type: RESET_ERRORS
+  };
+};
