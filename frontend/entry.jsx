e1f97acf16ca5097b028d2349dde4c8b12808beb
Philip <park.j.philip@gmail.com>
2017-07-31 00:02:24 -0400

    add static page controller, add store to frontend

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+console.log('abcwaefwef');
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById("root");
   ReactDOM.render(<h2>Welcome to Celmatix Challenge</h2>, root);
