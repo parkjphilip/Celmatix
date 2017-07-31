9ac073104c11133229094eface80900d9ce21b4d
Philip <park.j.philip@gmail.com>
2017-07-31 00:48:03 -0400

    outline front end routes

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -12,6 +12,14 @@ const Root = ({store}) => {
       <HashRouter>
         <App>
           <Switch>
+            <Route exact path="/" component={ ProductsContainer } />
+            <Route exact path="/login" component={ AuthFormContainer } />
+            <Route exact path="/signup" component={ AuthFormContainer } />
+            <Route exact path="/cart" component={ CartContainer }/>
+            <Route exact path="/orders" component={ CartContainer }/>
+            <Route exact path="/upload" component={ CartContainer }/>
+            <Route exact path="/addproducts" component={ CartContainer }/>
+            <Route exact path="/users" component={ CartContainer }/>
           </Switch>
         </App>
       </HashRouter>
