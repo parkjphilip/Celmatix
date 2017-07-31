afc1c1f80d3c88d7018f4ccc75c51f470cf38a56
Philip <park.j.philip@gmail.com>
2017-07-30 22:40:03 -0400

    initialize entry file

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,14 +15,14 @@ class User < ApplicationRecord
   attr_reader :password
 
   validates :email, :password_digest, :session_token, presence: true
-  validates :username, uniqueness: true
+  validates :email, uniqueness: true
   validates :password, length: {minimum: 6}, allow_nil: true
 
   after_initialize :ensure_session_token
 
   has_one :cart
   has_many :orders
-  
+
   def password=(password)
     self.password_digest = BCrypt::Password.create(password)
     @password = password
