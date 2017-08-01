2f842fe55c4dde0e92c2a9a4b927a1d1c189f738
Philip <park.j.philip@gmail.com>
2017-08-01 18:36:31 -0400

    add fetchcart action

--- frontend/actions/cart_actions.js
+++ frontend/actions/cart_actions.js
@@ -1,19 +1,19 @@
 import * as CartAPIUtil from '../util/cart_api_util';
 
-// export const RECEIVE_PRODUCTS = "RECEIVE_PRODUCTS";
+export const RECEIVE_CART = "RECEIVE_CART";
 
 export const addToCart = (productId, cartId, userId) => dispatch => {
   CartAPIUtil.addToCart(productId, cartId, userId);
 };
 
-// export const fetchProducts = () => dispatch => {
-//   return ProductAPIUtil.fetchProducts()
-//                        .then(products => dispatch(receiveProducts(products)));
-// };
-//
-// export const receiveProducts = products => {
-//   return ({
-//     type: RECEIVE_PRODUCTS,
-//     products
-//   });
-// };
+export const fetchCart = (cartId) => dispatch => {
+  return CartAPIUtil.fetchCart(cartId)
+                       .then(cart => dispatch(receiveCart(cart)));
+};
+
+export const receiveCart = cart => {
+  return ({
+    type: RECEIVE_CART,
+    cart
+  });
+};
