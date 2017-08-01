1f19f4dee50d302aa280ab6399a4a35abff34005
Philip <park.j.philip@gmail.com>
2017-08-01 18:26:37 -0400

    change default quantity value

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170801210501) do
+ActiveRecord::Schema.define(version: 20170801213156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cart_items", force: :cascade do |t|
-    t.integer  "product_id", null: false
-    t.integer  "quantity"
-    t.integer  "cart_id",    null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "product_id",             null: false
+    t.integer  "quantity",   default: 1
+    t.integer  "cart_id",                null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.index ["cart_id"], name: "index_cart_items_on_cart_id", unique: true, using: :btree
     t.index ["product_id"], name: "index_cart_items_on_product_id", unique: true, using: :btree
   end
