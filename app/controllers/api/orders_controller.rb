f96b9f83bb5dd2acf56a68e518d41c37ef3b26f8
Philip <park.j.philip@gmail.com>
2017-08-02 11:25:27 -0400

    refactor orders controller

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -4,15 +4,12 @@ class Api::OrdersController < ApplicationController
     @order = Order.new(user_id: Integer(params[:user_id]))
     @order.save
     @order.user.cart.cart_items.each do |cart_item|
-      debugger
+      @order_item = OrderItem.new(
+                                    product_id: cart_item.product_id,
+                                    order_id: @order.id
+                                 )
+      @order_item.save
     end
-    # params[:cart_items].each do |cart_item|
-    #   @order_item = OrderItem.new(
-    #                   product_id: Integer(params[:cart_items][cart_item]["product"]["id"]),
-    #                   order_id: @order.id
-    #                   )
-    #   @order_item.save!
-    # end
     render "api/orders/show"
   end
 end
