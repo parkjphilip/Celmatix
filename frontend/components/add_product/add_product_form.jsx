200183f3e4c7c41f099b1d7b35d28ea1935b0d01
Philip <park.j.philip@gmail.com>
2017-08-03 11:18:32 -0400

    add styling for add product form

--- frontend/components/add_product/add_product_form.jsx
+++ frontend/components/add_product/add_product_form.jsx
@@ -52,17 +52,17 @@ class AddProductForm extends React.Component {
 
 	render() {
 		return (
-      <div>
+      <div className="add-product-container">
           <form onSubmit={this.handleSubmit}>
-            <h1>Please fill out the form to add a product</h1>
+            <h1 className="add-product-text">Please fill out the form to add a product</h1>
             <div >
-              <input type="text" value={this.state.name} onChange={this.update("name")} placeholder="Product Name"/>
-              <input type="text" value={this.state.brand} onChange={this.update("brand")} placeholder="Brand"/>
-              <input type="text" value={this.state.model} onChange={this.update("model")} placeholder="Model"/>
-              <input type="text" value={this.state.sku} onChange={this.update("sku")} placeholder="Sku"/>
-              <input type="number" value={this.state.price} onChange={this.update("price")} placeholder="Price" step="0.01"/>
-              <input type="text" value={this.state.desc} onChange={this.update("desc")} placeholder="Description"/>
-              <input type="submit" value='Submit'/>
+              <input className="add-product-input" type="text" value={this.state.name} onChange={this.update("name")} placeholder="Product Name"/>
+              <input className="add-product-input" type="text" value={this.state.brand} onChange={this.update("brand")} placeholder="Brand"/>
+              <input className="add-product-input" type="text" value={this.state.model} onChange={this.update("model")} placeholder="Model"/>
+              <input className="add-product-input" type="text" value={this.state.sku} onChange={this.update("sku")} placeholder="Sku"/>
+              <input className="add-product-input" type="number" value={this.state.price} onChange={this.update("price")} placeholder="Price" step="0.01"/>
+              <input className="add-product-input-desc" type="text" value={this.state.desc} onChange={this.update("desc")} placeholder="Description"/>
+              <input className="add-product-submit hover" type="submit" value='Submit'/>
             </div>
           </form>
       </div>
